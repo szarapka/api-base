f3651db48e8ac6ed70a3afdaa9899ada39f01d3f
Scott Szarapka <scott@szarapka.com>
2016-01-27 23:13:32 -0800

    Moved Authentication method to API Controller

--- core/controllers/api.js
+++ core/controllers/api.js
@@ -4,14 +4,28 @@ var apiController
 
 apiController = (function() {
   // Dependencies
+  let _ = require('lodash')
+  let user = require('../models/user')
 
   function init() {
     // Initialize things privately.
   }
 
+  function createToken(user) {
+    let jwt = require('jsonwebtoken')
+    let secret = process.env.TOKEN_SECRET
+    let expiry = process.env.TOKEN_EXPIRY
+    let issuer = process.env.TOKEN_ISSUER
+
+    return jwt.sign(_.omit(user, 'password'), secret, {expiresIn: expiry, issuer: issuer})
+  }
+
   return {
     hello: (req, res, next) => {
       return res.status(200).json({msg: 'api-base'})
+    },
+    authenticate: (req, res, next) => {
+      // authenticate
     }
   }
 })()
